8a45d662cfcb8515b35f9f2c9a558e29e86c9e67
thnhtra <112228421+thnhtra@users.noreply.github.com>
2022-11-13 18:51:26 +0700

    ct275-03 b1910315

---
+++ contact.php
@@ -0,0 +1,47 @@
+<?php 
+	include "head.php"
+	?>
+<?php
+$title ="Shop huy";
+$name ="Điện thoai";
+?>
+<?php 
+	include "top.php"
+    ?>
+    <?php 
+	include "Header.php"
+	?>
+	<?php 
+	include "navigation.php"
+	?>
+	<!--//////////////////////////////////////////////////-->
+	<!--///////////////////Contact Page///////////////////-->
+	<!--//////////////////////////////////////////////////-->
+	<div id="page-content" class="single-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading"><h1>Liên hệ với chúng tôi</h1></div>
+				</div>
+				<div class="col-md-6">
+					<p><span class="glyphicon glyphicon-home"></span>235 Hoàng Quốc Việt, Cổ Nhuế, Từ Liêm, Hà Nội, Việt Nam.</p>
+					<p><span class="glyphicon glyphicon-earphone"></span> 012345678</p>
+					<p><span class="glyphicon glyphicon-envelope"></span> info.thanhtruong@gmail.com</p>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.8415184086525!2d105.76842661471188!3d10.02993369283066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0895a51d60719%3A0x9d76b0035f6d53d0!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBD4bqnbiBUaMah!5e0!3m2!1svi!2s!4v1668310879084!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php 
+	include "footer.php"
+	?>
+</body>
+</html>
